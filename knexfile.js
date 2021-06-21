4bca1e26b8369ebd62a86c466024af8d90e89fcd
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-06-21 15:53:02 -0300

    added: .env.example

--- knexfile.js
+++ knexfile.js
@@ -17,10 +17,10 @@ module.exports = {
     production: {
         client: 'mysql',
         connection: {
-            host: process.env.DB_HOST ? process.env.DB_HOST : "127.0.0.1",
-            user: process.env.DB_USER ? process.env.DB_USER : "root",
-            password: process.env.DB_PASS ? process.env.DB_PASS : "",
-            database: process.env.DB_BASE ? process.env.DB_BASE : "gerenciador_aplicacoes"
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_BASE
         },
         migrations: {
             directory: path.resolve(__dirname, 'database', 'migrations')
