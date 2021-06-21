d6681ac1b73e014d04d31c919894b7c7b16cb779
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-06-21 08:20:05 -0300

    added: Atualizar Ambiente

--- src/controllers/AmbienteController.ts
+++ src/controllers/AmbienteController.ts
@@ -95,5 +95,57 @@ routes.get('/ambiente/:id', async (req, res) => {
     res.send(resp);
 });
 
+routes.put('/ambiente/:id', async (req, res) => {
+    const { params, body } = req;
+    const resp = {
+        status: 0,
+        msg: '',
+        data: null,
+        errors: []
+    };
+
+    const ambienteGet = <IAmbiente> await Ambiente.GetFirst(`id = '${params.id}'`);
+    
+    if (ambienteGet === null) {
+        resp.errors.push({
+            msg: 'Ambiente não encontrada!'
+        });
+        return res.status(404).send(resp);
+    }
+
+    const data : { [k: string] : any} = {};
+    
+    const proibidos = [ 'id' ];
+    let edit = false;
+    
+    Ambiente.fields.forEach(campo => {
+        if (body[campo.name] !== undefined && !proibidos.includes(campo.name)) {
+            data[campo.name] = body[campo.name];
+            edit = true;
+        }
+    });
+
+    if (!edit) {
+        resp.errors.push({
+            msg: 'Nada para editar'
+        });
+        return res.status(400).send(resp);
+    }
+
+    const update = await Ambiente.Update(data, `id = '${params.id}'`);
+
+    if (update.status !== 1) {
+        resp.errors.push({
+            msg: 'Não foi possivel atualizar'
+        });
+
+        return res.status(500).send(resp);
+    }
+
+    resp.status = 1;
+    resp.msg = 'Atualizado com sucesso';
+    res.send(resp);
+});
+
 
 export default routes;
