1b6b9834f8f83abb2c176048445a707a6315c511
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-06-24 11:01:05 -0300

    updated: build script

--- docs/build.sh
+++ docs/build.sh
@@ -10,11 +10,10 @@ sed "s/@@DB_USER/@@DB_USER/g" -i .env
 sed "s/@@DB_PASS/@@DB_PASS/g" -i .env
 sed "s/@@DB_BASE/@@DB_BASE/g" -i .env
 
-export $(grep -v '^#' .env | xargs)
 
 npm i
 npm run build
 
 pm2 stop club7-feature-toggle-api
-pm2 start /home/deploy/apps/club7-feature-toggle-api/dist/index.js --name club7-feature-toggle-api -f
+pm2 start npm --name club7-feature-toggle-api -- start
 pm2 save
