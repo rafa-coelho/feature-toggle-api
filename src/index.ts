0380a610fa64dbaf316fc54383ac9c47a547f6c2
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-06-24 10:36:03 -0300

    added: teste

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const port = (process.env.NODE_PORT) ? process.env.NODE_PORT : 3333;
 
 app.listen(port, async () => {
     console.log('--------------------------------------------');
+    console.log(process.env.NODE_PORT);
     for (let index = 0; index < 10; index++) console.log('\n');
     console.clear();
     console.log(`Rodando na porta ${port}`);
