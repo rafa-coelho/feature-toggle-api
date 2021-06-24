8298e581738d3e8935e7f060724b681095ee9a4f
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-06-24 09:52:37 -0300

    added: build

---
+++ docs/build.sh
@@ -0,0 +1,17 @@
+cd /home/deploy/apps/club7-feature-toggle-api
+
+cp .env.example .env
+
+sed "s/@@NODE_ENV/false/g" -i .env
+
+sed "s/@@DB_HOST/@@DB_HOST/g" -i .env
+sed "s/@@DB_USER/@@DB_USER/g" -i .env
+sed "s/@@DB_PASS/@@DB_PASS/g" -i .env
+sed "s/@@DB_BASE/@@DB_BASE/g" -i .env
+
+npm i
+npm run build
+
+pm2 stop club7-feature-toggle-api
+pm2 start /home/deploy/apps/club7-feature-toggle-api/dist/index.js --name club7-feature-toggle-api -f
+pm2 save
